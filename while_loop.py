0e3b83c41718722df7d5fd27779e5786fb40a6fb
Maia <maia.groundwatercapital@gmail.com>
2020-10-06 14:42:13 -0400

    while loop

---
+++ while_loop.py
@@ -0,0 +1,21 @@
+# regular while loop
+a = 0
+while a < 5:
+    print(a)
+    a += 1
+
+# while/else statement
+a = 0
+while a < 5:
+    print(a)
+    a += 1
+else:
+    print(f'This is when the while loop end. a = {a}')
+
+# sometime, we want to do the loop first at least once before evaluate the condition to exit the loop
+a = 0
+while True:
+    print(a)
+    a += 1
+    if a >= 5:
+        break
